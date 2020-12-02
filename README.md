df6f2b6b5b459087a75b93286fc9681111f07f2b
Dzmitry <dzmchars@gmail.com>
2020-12-02 07:53:27 +0300

    main refactor

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ Create `.png` images with ease
 | -width=100    | 0             | -           |
 | -hegiht=100   | 0             | -           |
 
+
+Read from file:
+```bash
+mkpng -file=images.txt
+```
+
+`images.txt`:
+```
+1920x1080
+1024x768
+```
--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 	"os"
 )
 
+// DefaultFilenameTemplate is a mask for generating image file names
+const DefaultFilenameTemplate = "image-%d-%d.png"
+
 func main() {
 	width := flag.Int("width", 0, "width of the image")
 	height := flag.Int("height", 0, "height of the image")
@@ -20,8 +23,14 @@ func main() {
 		log.Fatal("Zero values are not allowed")
 	}
 
+	img := createImage(*width, *height)
+	encodeImage(*width, *height, img)
+
+}
+
+func createImage(width int, height int) *image.RGBA {
 	upLeft := image.Point{0, 0}
-	lowRight := image.Point{*width, *height}
+	lowRight := image.Point{width, height}
 
 	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
 
@@ -29,20 +38,23 @@ func main() {
 	cyan := color.RGBA{100, 200, 200, 0xff}
 
 	// Set color for each pixel.
-	for x := 0; x < *width; x++ {
-		for y := 0; y < *height; y++ {
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
 			switch {
-			case x < *width/2 && y < *height/2: // upper left quadrant
+			case x < width/2 && y < height/2: // upper left quadrant
 				img.Set(x, y, cyan)
-			case x >= *width/2 && y >= *height/2: // lower right quadrant
+			case x >= width/2 && y >= height/2: // lower right quadrant
 				img.Set(x, y, color.White)
 			default:
 				// Use zero value.
 			}
 		}
 	}
+	return img
+}
 
+func encodeImage(width int, height int, img *image.RGBA) {
 	// Encode as PNG.
-	f, _ := os.Create(fmt.Sprintf("image-%d-%d.png", *width, *height))
+	f, _ := os.Create(fmt.Sprintf(DefaultFilenameTemplate, width, height))
 	png.Encode(f, img)
 }
